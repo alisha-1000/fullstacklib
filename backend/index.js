f84c4decd6bd7365182fadff624dc289cfd98ca7
Alisha Gupta <alishagupta2004gupta@gmail.com>
2025-12-05 16:14:39 +0530

    done

--- backend/index.js
+++ backend/index.js
@@ -9,11 +9,7 @@ const app = express();
 connectDB();
 
 // Middlewares
-app.use(cors({
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-}));
+app.use(cors());
 
 app.use(express.json());
 
